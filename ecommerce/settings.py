709d1e66a012e3b2dfedd5813e506a701ac1c25b
Sumaira <sumaira@example.com>
2026-09-19 23:05:08 +0500

    Fix static files issue

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -151,7 +151,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
